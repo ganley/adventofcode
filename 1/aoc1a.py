b20e722efa19ef6b07b7de8edbff8e029b82dfee
Joe Ganley <joe@ganley.org>
2020-12-04 16:37:47 -0500

    Switched to f-strings, which I just learned about and which are awesome.

--- 1/aoc1a.py
+++ 1/aoc1a.py
@@ -6,6 +6,6 @@ with open(sys.argv[1], "r") as f:
 
 for a,b in itertools.combinations(nums, 2):
     if a + b == 2020:
-        print("{a} + {b} = 2020".format(a=a, b=b))
-        print("{a} * {b} = {prod}".format(a=a, b=b, prod=a * b))
+        print(f"{a} + {b} = 2020")
+        print(f"{a} * {b} = {a*b}")
 
--- 1/aoc1b.py
+++ 1/aoc1b.py
@@ -6,6 +6,6 @@ with open(sys.argv[1], "r") as f:
 
 for a,b,c in itertools.combinations(nums, 3):
     if a + b + c == 2020:
-        print("{a} + {b} + {c} = 2020".format(a=a, b=b, c=c))
-        print("{a} * {b} * {c} = {prod}".format(a=a, b=b, c=c, prod=a * b * c ))
+        print(f"{a} + {b} + {c} = 2020")
+        print(f"{a} * {b} * {c} = {a*b*c}")
 
