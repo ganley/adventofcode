e8e5d0ced4218408f7e6095642cc421767354a61
Joe Ganley <joe@ganley.org>
2020-12-04 09:04:00 -0500

    Realized several checks could be done within their regex.

--- 4/aoc4b.py
+++ 4/aoc4b.py
@@ -4,29 +4,21 @@ import sys
 
 
 regexs = {
-    "byr": "^\d{4}$",
-    "iyr": "^\d{4}$",
-    "eyr": "^\d{4}$",
-    "hgt": "^\d+(cm|in)$",
-    "hcl": "^\#[\dA-Fa-f]{6}$",
-    "ecl": "^[a-z]{3}$",
-    "pid": "^\d{9}$"
+    "byr": "(19[2-9]\d|200[0-2])$",
+    "iyr": "(201\d|2020)$",
+    "eyr": "(202\d|2030)$",
+    "hgt": "\d+(cm|in)$",
+    "hcl": "\#[\dA-Fa-f]{6}$",
+    "ecl": "(amb|blu|brn|gry|grn|hzl|oth)$",
+    "pid": "\d{9}$"
 }
+
 
 
 def check(fields):
     for k,r in regexs.items():
         if not (k in fields and re.match(r, fields[k])):
             return 0
-
-    if not 1920 <= int(fields["byr"]) <= 2002:
-        return 0
-
-    if not 2010 <= int(fields["iyr"]) <= 2020:
-        return 0
-
-    if not 2020 <= int(fields["eyr"]) <= 2030:
-        return 0
 
     hgt = int(fields["hgt"][:-2])
     units = fields["hgt"][-2:]
@@ -37,9 +29,6 @@ def check(fields):
         if not 59 <= hgt <= 76:
             return 0
     else:
-        return 0
-
-    if not fields["ecl"] in [ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" ]:
         return 0
 
     return 1
