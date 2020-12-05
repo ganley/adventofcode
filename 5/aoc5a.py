ad778dadd5a6700093bbec44feea3bd1e39ab1f1
Joe Ganley <joe@ganley.org>
2020-12-05 06:53:09 -0500

    Just learned about autopep8.

--- 5/aoc5a.py
+++ 5/aoc5a.py
@@ -1,19 +1,10 @@
 import sys
 
 
-
 def tr(string, fromchars, tochars):
-    return ''.join([ tochars[fromchars.index(c)] for c in string ])
-
+    return ''.join([tochars[fromchars.index(c)] for c in string])
 
 
 with open(sys.argv[1], "r") as f:
-    seatids = [ int(tr(line.strip(), "FBLR", "0101"), 2) for line in f ]
+    seatids = [int(tr(line.strip(), "FBLR", "0101"), 2) for line in f]
     print(max(seatids))
-
-
-
-
-
-
-
--- 5/aoc5b.py
+++ 5/aoc5b.py
@@ -1,16 +1,13 @@
 import sys
 
 
-
 def tr(string, fromchars, tochars):
-    return ''.join([ tochars[fromchars.index(c)] for c in string ])
-
+    return ''.join([tochars[fromchars.index(c)] for c in string])
 
 
 with open(sys.argv[1], "r") as f:
-    seatids = set([ int(tr(line.strip(), "FBLR", "0101"), 2) for line in f ])
+    seatids = set([int(tr(line.strip(), "FBLR", "0101"), 2) for line in f])
 
     for i in range(1, 1023):
         if not i in seatids and (i - 1) in seatids and (i + 1) in seatids:
             print(i)
-
