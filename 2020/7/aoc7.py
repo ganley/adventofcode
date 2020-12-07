ec785c4e192c3660a79f4a29ac343f668def6b0b
joe.ganley <jganley@novetta.com>
2020-12-07 11:01:29 -0500

    Be more consistent at separating words with underscores.

--- 2020/7/aoc7.py
+++ 2020/7/aoc7.py
@@ -1,66 +1,66 @@
 import sys
 
-# parent (downward): parent color -> { child_color: n }
-# child (upward): child color ->  { parent color : n }
+# parent (downward): parent_color -> { child_color: n }
+# child (upward): child_color ->  { parent_color : n }
 
-# returns (parent graph, child graph)
+# returns (parent_graph, child_graph)
 def build_graph(filename):
     with open(filename, "r") as f:
         child = {}
         parent = {}
         for line in f:
             tok = line.split()
-            parentcolor = tok.pop(0) + "_" + tok.pop(0)
+            parent_color = tok.pop(0) + "_" + tok.pop(0)
             assert tok.pop(0) == "bags" and tok.pop(0) == "contain"
 
-            assert not parentcolor in parent
-            parent[parentcolor] = {}
+            assert not parent_color in parent
+            parent[parent_color] = {}
 
             while tok:
-                numstr = tok.pop(0)
-                if num := 0 if numstr == "no" else int(numstr):
-                    childcolor = tok.pop(0) + "_" + tok.pop(0)
-                    parent[parentcolor][childcolor] = num
+                num_str = tok.pop(0)
+                if num := 0 if num_str == "no" else int(num_str):
+                    child_color = tok.pop(0) + "_" + tok.pop(0)
+                    parent[parent_color][child_color] = num
                 else:
-                    childcolor = tok.pop(0)
-                    assert childcolor == "other"
+                    child_color = tok.pop(0)
+                    assert child_color == "other"
 
-                assert (num == 0 and childcolor == "other") or num > 0
+                assert (num == 0 and child_color == "other") or num > 0
                 assert tok.pop(0).startswith("bag")
 
-                if childcolor != "other":
-                    if not childcolor in child:
-                        child[childcolor] = {}
-                    child[childcolor][parentcolor] = num
+                if child_color != "other":
+                    if not child_color in child:
+                        child[child_color] = {}
+                    child[child_color][parent_color] = num
 
     return (parent, child)
 
 
 
-def ancestors(childgraph, color):
+def ancestors(child_graph, color):
     anc = []
-    for parentcolor in childgraph.get(color, {}):
-        anc.append(parentcolor)
-        anc += ancestors(childgraph, parentcolor)
+    for parent_color in child_graph.get(color, {}):
+        anc.append(parent_color)
+        anc += ancestors(child_graph, parent_color)
     return list(set(anc))
 
 
 
 # do *not* count the outermost bag
-def total_bags_contained(parentgraph, color):
+def total_bags_contained(parent_graph, color):
     weight = 0
-    for childcolor,w in parentgraph.get(color, {}).items():
+    for child_color,w in parent_graph.get(color, {}).items():
         weight += w
-        weight += w * total_bags_contained(parentgraph, childcolor)
+        weight += w * total_bags_contained(parent_graph, child_color)
     return weight
 
 
 
-parentgraph,childgraph = build_graph(sys.argv[1])
+parent_graph,child_graph = build_graph(sys.argv[1])
 
 # part 1:
-print(len(ancestors(childgraph, "shiny_gold")))
+print(len(ancestors(child_graph, "shiny_gold")))
 
 # part 2:
-print(total_bags_contained(parentgraph, "shiny_gold"))
+print(total_bags_contained(parent_graph, "shiny_gold"))
 
