243c615f4797929aa42d168fcdfbde1ce5c93e78
joe.ganley <jganley@novetta.com>
2020-12-07 11:09:27 -0500

    Code skeleton to start each day with.

---
+++ 2020/aoc_skeleton.py
@@ -0,0 +1,18 @@
+import functools
+import itertools
+import re
+import string
+import sys
+
+
+
+if __name__ == "__main__":
+    pass
+
+#    with open(sys.argv[1], "r") as f:
+#        for rawline in f:
+#            if line := rawline.strip():
+#                # handle nonblank line
+#            else:   # blank line
+#                # handle blank line
+
