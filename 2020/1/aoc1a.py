ee2eafbdbb0b82d67dde5100787b3ebfa67a2522
joe.ganley <jganley@novetta.com>
2020-12-09 08:36:35 -0500

    Autopep8.

--- 2020/1/aoc1a.py
+++ 2020/1/aoc1a.py
@@ -2,10 +2,9 @@ import itertools
 import sys
 
 with open(sys.argv[1], "r") as f:
-    nums = [ int(i) for i in f.read().split() ]
+    nums = [int(i) for i in f.read().split()]
 
-for a,b in itertools.combinations(nums, 2):
+for a, b in itertools.combinations(nums, 2):
     if a + b == 2020:
         print(f"{a} + {b} = 2020")
         print(f"{a} * {b} = {a*b}")
-
--- 2020/1/aoc1b.py
+++ 2020/1/aoc1b.py
@@ -2,10 +2,9 @@ import itertools
 import sys
 
 with open(sys.argv[1], "r") as f:
-    nums = [ int(i) for i in f.read().split() ]
+    nums = [int(i) for i in f.read().split()]
 
-for a,b,c in itertools.combinations(nums, 3):
+for a, b, c in itertools.combinations(nums, 3):
     if a + b + c == 2020:
         print(f"{a} + {b} + {c} = 2020")
         print(f"{a} * {b} * {c} = {a*b*c}")
-
