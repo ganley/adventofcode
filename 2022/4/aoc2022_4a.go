09f450ab90a3aac469ee0fd781f35c3aca505898
Joe Ganley <joe@ganley.org>
2022-12-14 21:31:25 -0500

    Half of day 4

---
+++ 2022/4/aoc2022_4a.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "regexp"
+    "strconv"
+)
+
+
+func main() {
+    var re = regexp.MustCompile("[-,]")
+    var c = 0
+
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        var t = re.Split(scanner.Text(), 4)
+        var s0, _ = strconv.Atoi(t[0])
+        var e0, _ = strconv.Atoi(t[1])
+        var s1, _ = strconv.Atoi(t[2])
+        var e1, _ = strconv.Atoi(t[3])
+        if (s0 <= s1 && e1 <= e0) || (s1 <= s0 && e0 <= e1) {
+            c++
+        }
+    }
+
+    fmt.Println(c)
+}
+
